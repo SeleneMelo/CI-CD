f2f686f92c7354b49be74f07d5decc1c023b4f48
selenemelo <selenemelo2000@gmail.com>
2023-11-24 21:08:03 -0300

    Resolve conflitos e atualiza o codigo

--- teste_script.py
+++ teste_script.py
@@ -1,13 +1,13 @@
 import unittest
-from script import add_numbers, multiply_numbers
+from script import add_numbers, mul_numbers
 
 class TestProgram(unittest.TestCase):
     def test_add_numbers(self):
         result = add_numbers(3, 5)
         self.assertEqual(result, 8)
 
-    def test_multiply_numbers(self):
-        result = multiply_numbers(3, 5)
+    def test_mul_numbers(self):
+        result = mul_numbers(3, 5)
         self.assertEqual(result, 15)
 
 if __name__ == '__main__':
