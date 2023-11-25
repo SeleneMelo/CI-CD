fe6313143a4f63c4921a79d9ff3487f92250aa71
selenemelo <selenemelo2000@gmail.com>
2023-11-24 21:17:23 -0300

    Adc mais testes de unidade

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
-def add_numbers(a, b):
-    return a + b
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
 
-def mul_numbers(a,b):
-    return a * b
+    fib_sequence = [0, 1]
+    for _ in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    return fib_sequence
--- teste_script.py
+++ teste_script.py
@@ -1,14 +1,37 @@
 import unittest
-from script import add_numbers, mul_numbers
+from script import fibonacci
 
 class TestProgram(unittest.TestCase):
-    def test_add_numbers(self):
-        result = add_numbers(3, 5)
-        self.assertEqual(result, 8)
+    def test_fibonacci_zero(self):
+        result = fibonacci(0)
+        self.assertEqual(result, [])
 
-    def test_mul_numbers(self):
-        result = mul_numbers(3, 5)
-        self.assertEqual(result, 15)
+    def test_fibonacci_one(self):
+        result = fibonacci(1)
+        self.assertEqual(result, [0])
+
+    def test_fibonacci_two(self):
+        result = fibonacci(2)
+        self.assertEqual(result, [0, 1])
+
+    def test_fibonacci_positive(self):
+        result = fibonacci(6)
+        self.assertEqual(result, [0, 1, 1, 2, 3, 5])
+
+    def test_fibonacci_negative(self):
+        # Teste para garantir que a função lida bem com entradas negativas
+        result = fibonacci(-3)
+        self.assertEqual(result, [])
+
+    def test_fibonacci_large_input(self):
+        # Teste para garantir que a função lida bem com entradas grandes
+        result = fibonacci(15)
+        self.assertEqual(result, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+
+    def test_fibonacci_string_input(self):
+        # Teste para garantir que a função lida bem com entradas não numéricas
+        result = fibonacci("abc")
+        self.assertEqual(result, [])
 
 if __name__ == '__main__':
     unittest.main()
